62628e6851d2e38c2a120e00d55cac93a573d320
Linsen HU <1563883475@qq.com>
2025-10-04 22:09:11 +0800

    feat: commend-allowed

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,9 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
+import { commentPlugin } from '@vuepress/plugin-comment'
+
+
 export default defineUserConfig({
   base: "/",
   locales: {
@@ -17,7 +20,17 @@ export default defineUserConfig({
   },
 
   theme,
-
+  plugins: [
+    commentPlugin({
+      provider: 'Giscus', // Artalk | Giscus | Waline | Twikoo
+      repo: "GALA-Lin/GALA-Lin.github.io", // 仓库名
+      repoId: "R_kgDOO7liCw", // 仓库ID
+      category: "Announcements", // 分类
+      categoryId: "DIC_kwDOO7liC84CwPgD", // 分类ID
+      lazyLoading: true, // 延迟加载
+      // 服务商特定选项
+    }),
+  ],
   // Enable it with pwa
   // shouldPrefetch: false,
 });
